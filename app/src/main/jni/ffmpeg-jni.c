c54f3d5e4de31f1a91c765d418e50c22778d7ceb
ixlei <=>
2017-04-25 20:47:17 +0800

    ffmpeg add

---
+++ app/src/main/jni/ffmpeg-jni.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "libavformat/avformat.h"
+#include <libavfilter/avfilter.h>
+#include "libavcodec/avcodec.h"
+#include <jni.h>
+JNIEXPORT jstring Java_com_example_yuchen_helloworld_MainActivity_avcodecInfo(JNIEnv* env, jobject obj)
+{
+    char info[4000] = { 0 };
+    int count = 100;  //输出前100个codec名字
+    
+    av_register_all();//初始化所有decoder和encoder,注册所有容器类型和codec
+    char s[1000] = { 0 };
+    sprintf(s, "%s\n", avcodec_configuration());
+    AVCodec *c_temp = av_codec_next(NULL);
+    while (c_temp != NULL && count > 0){
+        //输出解码器和编码器
+        if(c_temp->decode != NULL){
+            sprintf(info,"%s[Decoder]",info);
+        }
+        else{
+            sprintf(info,"%s[Enc]",info);
+        }
+        
+        sprintf(info,"%s[%10s]\n",info,c_temp->name);
+        
+        c_temp = c_temp->next;
+        count--;
+    }
+
+    return (*env)->NewStringUTF(env, info);
+ }
+
+
+
+ JNIEXPORT jstring Java_com_example_yuchen_helloworld_MainActivity_avcodecMsg(JNIEnv* env, jobject obj)
+{
+    char s[1000] = { 0 };
+    sprintf(s, "%s\n", "hello ffmpeg world");
+    return (*env)->NewStringUTF(env, s);
+ }
---
+++ app/src/main/obj/local/armeabi/objs/ffmpeg-jni/ffmpeg-jni.o.d
@@ -0,0 +1,80 @@
+/Users/yuchen/androidProject/ilive-android/app/src/main/obj/local/armeabi/objs/ffmpeg-jni/ffmpeg-jni.o: \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/ffmpeg-jni.c \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavformat/avformat.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavcodec/avcodec.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/samplefmt.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/avutil.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/common.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/attributes.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/macros.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/version.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/avconfig.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/mem.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/error.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/rational.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/mathematics.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/intfloat.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/log.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/pixfmt.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/buffer.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/cpu.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/channel_layout.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/dict.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/frame.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavcodec/version.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavformat/avio.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavformat/version.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavfilter/avfilter.h \
+  /Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavfilter/version.h
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavformat/avformat.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavcodec/avcodec.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/samplefmt.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/avutil.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/common.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/attributes.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/macros.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/version.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/avconfig.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/mem.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/error.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/rational.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/mathematics.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/intfloat.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/log.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/pixfmt.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/buffer.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/cpu.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/channel_layout.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/dict.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavutil/frame.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavcodec/version.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavformat/avio.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavformat/version.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavfilter/avfilter.h:
+
+/Users/yuchen/androidProject/ilive-android/app/src/main/jni/include/libavfilter/version.h:
