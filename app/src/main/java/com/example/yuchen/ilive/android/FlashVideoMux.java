7005375e8d7f496b39fd38f442032f424ee70a09
ixlei <=>
2017-05-05 23:11:02 +0800

    flv mux

--- app/src/main/java/com/example/yuchen/ilive/android/FlashVideoMux.java
+++ app/src/main/java/com/example/yuchen/ilive/android/FlashVideoMux.java
@@ -33,4 +33,128 @@ public class FlashVideoMux {
         header[8] = 0x09;
         return header;
     }
+
+    /**
+     *
+     * @param tagType enum{0x08, 0x09, 0x12}
+     * @param filter {0x00}
+     * @param timestamp
+     * @param timestampExtended PTS = timestamp | timestampExtended << 24
+     * @return
+     */
+
+    public ByteBuffer muxTagHeader(int tagType, int dataLength, int timestamp, int timestampExtended, int filter) {
+        ByteBuffer tagHeader = ByteBuffer.allocate(11);
+        int headerFirstByte = ((filter << 6) | tagType);
+        //frame type and data length
+        int dataLenAndType = ((dataLength & 0x00FFFFFF) | ((headerFirstByte & 0xFF) << 24));
+        tagHeader.putInt(dataLenAndType);
+        //
+        int pts = (((timestamp << 8) & 0xFFFFFF00) | (timestampExtended & 0x000000FF));
+        tagHeader.putInt(pts);
+        //StreamID
+        for(int i = 8; i < 11; i++) {
+            tagHeader.put((byte)0x00);
+        }
+
+        return tagHeader;
+    }
+
+    public byte[] convertIntToByte(int value) {
+        return ByteBuffer.allocate(4).putInt(1680).array();
+    }
+
+    public byte[] muxAudioTag(int soundType, double soudRate, int channelCount, int sampleBit, boolean isSequenceHeader) {
+        byte[] audioHeader = new byte[2];
+        //if sound type is not aac, audio header have AACPacketType
+        if(soundType != 10) {
+            audioHeader = new byte[1];
+        }
+
+        int soundSize = sampleBit == 8 ? 0 : 1;
+        int SoundType = channelCount == 1 ? 0 : 1;
+
+        audioHeader[0] = (byte) (((soundType & 0x0F)<< 4)
+                | ((getSoundRate(soudRate) & 0x03) << 2)
+                | ((soundSize & 0x01)<< 1)
+                | (SoundType & 0x01));
+        if(soundType == 10) {
+            audioHeader[1] = (byte) (isSequenceHeader ? 0 : 1);
+        }
+        return audioHeader;
+    }
+
+    public byte[] muxSequenceAudioInfo(int sampleRate, int channelCount) {
+        int sampleRateIndex = getSampleIndexAudioRate(sampleRate);
+        byte[] sequenceAudioInfo = new byte[2];
+        sequenceAudioInfo[0] = (byte) (0x10 | ((sampleRateIndex >> 1) & 0x7));
+        sequenceAudioInfo[1] = (byte) (((sampleRateIndex & 0x1)<<7) | ((channelCount & 0xF) << 3));
+        return sequenceAudioInfo;
+    }
+
+    public byte[] muxVideoTagHeader(int frameType, int codecID) {
+        byte[] videoTagHeader = new byte[1];
+        videoTagHeader[0] = (byte) (((frameType & 0x0F) << 4) | (codecID & 0x0F));
+        return videoTagHeader;
+    }
+
+    public ByteBuffer muxAVCTagHeader(int frametype, int AVCPacketType, int compositionTime) {
+        ByteBuffer avcTagHeader = ByteBuffer.allocate(5);
+        avcTagHeader.put(muxVideoTagHeader(frametype, 7));
+        int avcPacketTypeAndCompTime = (((AVCPacketType & 0x000000FF) << 24) | (compositionTime & 0x00FFFFFF));
+        avcTagHeader.putInt(avcPacketTypeAndCompTime);
+        return avcTagHeader;
+    }
+
+    /**
+     *  http://befo.io/4178.html
+     * @param sps h264 sps
+     * @param pps h264 pps
+     * @return
+     */
+    public ByteBuffer muxVideoFirstTag(byte[] sps, byte[] pps) {
+        ByteBuffer videoFirstTag = ByteBuffer.allocate(11);
+        //1
+        videoFirstTag.put((byte)0x01);
+        videoFirstTag.put(sps[1]);
+        videoFirstTag.put(sps[2]);
+        videoFirstTag.put(sps[3]);
+        videoFirstTag.put((byte)0xFF);
+
+        videoFirstTag.put((byte)0xe1);
+        videoFirstTag.putShort((short)sps.length);
+        videoFirstTag.put(sps);
+
+        videoFirstTag.put((byte)0x01);
+        videoFirstTag.putShort((short)pps.length);
+        videoFirstTag.put(pps);
+        return videoFirstTag;
+    }
+
+    
+
+    public int getSoundRate(double val) {
+        if(val == 5.5) {
+            return 0;
+        }
+        if(val == 11) {
+            return 1;
+        }
+        if(val == 22) {
+            return 2;
+        }
+        if(val == 44) {
+            return 3;
+        }
+        return 3;
+    }
+
+    public int getSampleIndexAudioRate(int sampleRate) {
+        switch (sampleRate) {
+            case 96000: return 0;
+            case 44100 : return 4;
+            default: return 4;
+        }
+    }
+
 }
--- app/src/test/java/com/example/yuchen/ilive/android/ExampleUnitTest.java
+++ app/src/test/java/com/example/yuchen/ilive/android/ExampleUnitTest.java
@@ -5,6 +5,8 @@ import android.util.Log;
 
 import org.junit.Test;
 
+import java.nio.ByteBuffer;
+
 import static org.junit.Assert.*;
 
 /**
@@ -28,4 +30,40 @@ public class ExampleUnitTest {
             assertEquals(header[i], expectHeader[i]);
         }
     }
+
+    @Test
+    public void falshVideoTagHeader() {
+
+        byte[] header = flashVideoMux.muxTagHeader(9, 1680, 16, 0, 0);
+        byte[] res = flashVideoMux.convertIntToByte(1680);
+        System.out.println(res[3]);
+        System.out.println((int)res[2]);
+        System.out.println((int)res[1]);
+        System.out.println((byte)1680);
+        ByteBuffer bb = ByteBuffer.allocate(11);
+        bb.putInt(1222);
+        bb.putInt(37833);
+        bb.put((byte)0);
+        bb.put((byte)0);
+        bb.put((byte)0);
+        //bb.put((byte)0);
+
+
+        byte[] expectHeader = new byte[]{0x09, 0x00, res[2], res[3], 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00};
+        //assertArrayEquals(header, expectHeader);
+        System.out.print(ByteBuffer.allocate(4).putInt(1680).limit());
+    }
+
+    @Test
+    public void flashVideoConvertIntToByte() {
+        byte[] res = flashVideoMux.convertIntToByte(0);
+        for(int i = 0; i < 4; i++) {
+          //  assertEquals(res[i], 0x00);
+        }
+        res = flashVideoMux.convertIntToByte(18);
+        byte[] expectArr = new byte[] {0x00,0x00, 0x00, 0x12};
+        //assertArrayEquals(res, expectArr);
+
+    }
+
 }
